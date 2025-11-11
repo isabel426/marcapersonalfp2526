3b432508f4b27611369081172ebe97a0493a803b
Isabel <2913343@alu.murciaeduca.es>
2025-11-11 09:43:35 +0100

    Varias vistas con dopetrope

--- resources/views/users/usersList.blade.php
+++ resources/views/users/usersList.blade.php
@@ -1,5 +1,9 @@
+@extends('dopetrope.master')
+
+@section('content')
 <ul>
     @foreach ($users as $user)
         <li>Usuario {{ $user['name'] }} con identificador: {{ $user['id'] }}</li>
     @endforeach
 </ul>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,18 @@ Route::get('/', function () {
    return view('home');
 });
 
+Route::get('/users', function () {
+   return view('users.usersList', [
+    'users' => [
+        ['id' => 1, 'name' => 'Ana'],
+        ['id' => 2, 'name' => 'Luis'],
+        ['id' => 3, 'name' => 'Miguel'],
+        ['id' => 4, 'name' => 'Juan'],
+
+
+    ]
+   ]);
+});
 
 // ----------------------------------------
 Route::get('login', function () {
