956c9a7b8df2fefb0ed1cfd9d35ffffe50ce1b0d
Isabel <2913343@alu.murciaeduca.es>
2026-01-15 10:03:24 +0100

    Añadiendo PHP-CRUD_API

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,10 @@ return Application::configure(basePath: dirname(__DIR__))
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
 
+        $middleware->validateCsrfTokens(except: [
+            'http://marcapersonalfp.test/api/records/*',
+        ]);
+
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
